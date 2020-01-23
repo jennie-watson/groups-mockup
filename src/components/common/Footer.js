f7bb2c4af74e236dc74e86496bfa36f622524344
Jennie Watson <jennie.watson@fairfaxmedia.co.nz>
2020-01-23 16:21:09 +1300

    prettier applied to Footer.js

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,25 +1,27 @@
 import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className="footer page-container d-flex flex-column justify-content-center">
-        <span className="copyright">&copy; Neighbourly {1900 + new Date().getYear()}</span>
-        <nav>
-          <a href="/about-us">About</a>
-          <a href="/mobile">Mobile</a>
-          <a href="/about-us#privacy">Privacy</a>
-          <a href="/about-us#safety">Safety</a>
-          <a href="/about-us#guidelines">Guidelines</a>
-          <a href="/help">Help</a>
-          <a href="/terms-and-conditions">Terms</a>
-          <a href="/community/lead">Leads</a>
-          <a href="/community/organisation">Organisations</a>
-          <a href="/business/">Businesses</a>
-          <a href="/contact-us">Contact us</a>
-          <a href="/blog">Blog</a>
-        </nav>
-      </footer>
-    )
+  return (
+    <footer className="footer page-container d-flex flex-column justify-content-center">
+      <span className="copyright">
+        &copy; Neighbourly {1900 + new Date().getYear()}
+      </span>
+      <nav>
+        <a href="/about-us">About</a>
+        <a href="/mobile">Mobile</a>
+        <a href="/about-us#privacy">Privacy</a>
+        <a href="/about-us#safety">Safety</a>
+        <a href="/about-us#guidelines">Guidelines</a>
+        <a href="/help">Help</a>
+        <a href="/terms-and-conditions">Terms</a>
+        <a href="/community/lead">Leads</a>
+        <a href="/community/organisation">Organisations</a>
+        <a href="/business/">Businesses</a>
+        <a href="/contact-us">Contact us</a>
+        <a href="/blog">Blog</a>
+      </nav>
+    </footer>
+  );
 };
 
 export default Footer;
